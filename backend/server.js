30cc201e0787ffe0458cb18de9362026214f6140
amulyamandala <amulyamandala007@gmail.com>
2026-05-14 14:49:20 +0530

    fixed

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import { config } from 'dotenv'
 config();
 const app = exp()
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://employee-management-three-pied.vercel.app']
+    origin: ['http://localhost:5173', 'https://employee-management-fwp5m7uxy.vercel.app', 'https://employee-management-three-pied.vercel.app']
 }))
 
 //add body parser why
